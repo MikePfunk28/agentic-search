24ee7db7c873d2858d5c4f77619657b433ca393a
MikePfunk28 <mike.pfundt@gmail.com>
2025-11-08 20:14:25 -0500

    fix: Remove @cloudflare/vite-plugin and update wrangler configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,21 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
+    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart(),
     viteReact(),
   ],
+  ssr: {
+    target: 'webworker',
+    noExternal: true,
+  },
 })
 
 export default config
